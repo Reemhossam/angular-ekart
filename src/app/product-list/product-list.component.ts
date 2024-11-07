7ca2e83d41b00773ba65139f156850a5e36293f6
Reemhossam <reemhossam1997@gmail.com>
2024-11-07 16:48:16 +0200

    add news

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+name:string="Reem Hossam";
 product=
 {
 name : "iphone 16",
@@ -17,5 +18,8 @@ pImage:"../../assets/1727880149116.jpg"
 }
 getdiscountedprice(){
   return (this.product.price - (this.product.price * this.product.discount/100))
+}
+onNameChange(event:any){
+  this.name =event.target.value;
 }
 }
